bb5cf2e4b733432585a381a8be049752e27a9563
rsarm <rafael.sarmiento@cscs.ch>
2022-09-12 09:08:15 +0200

    Update run-singularity-aws.sh

--- pytorch/ptdist/run-singularity-aws.sh
+++ pytorch/ptdist/run-singularity-aws.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=cnn-pytorch
 #SBATCH --ntasks=4
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=8
 #SBATCH --gpus-per-node=8
 #SBATCH --time=0:10:0
 #SBATCH --partition gpu
@@ -12,12 +12,13 @@ module load partition/G
 module load singularity-bindings
 module load rccl
 module load aws-ofi-rccl
+module load OpenMPI
 
 export NCCL_SOCKET_IFNAME=hsn0
 export NCCL_NET_GDR_LEVEL=3
 export SINGULARITYENV_LD_LIBRARY_PATH=/opt/ompi/lib:/opt/rocm-5.0.1/lib:$EBROOTAWSMINOFIMINRCCL/lib:/opt/cray/xpmem/2.4.4-2.3_9.1__gff0e1d9.shasta/lib64:$SINGULARITYENV_LD_LIBRARY_PATH
 
-srun singularity exec \
+mpirun singularity exec \
                  -B"/appl:/appl" 
                  -B"$EBROOTRCCL/lib/librccl.so.1.0:/opt/rocm-5.0.1/rccl/lib/librccl.so.1.0.50001" \
                  deepspeed_rocm5.0.1_ubuntu18.04_py3.7_pytorch_1.10.0.sif \
