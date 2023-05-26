9300eecab638997cef0aa5fa99758394e9426f28
rsarm <rafael.sarmiento@cscs.ch>
2023-05-26 11:32:36 +0200

    Update install-horovod.sh

--- tensorflow/hvd/install-horovod.sh
+++ tensorflow/hvd/install-horovod.sh
@@ -8,7 +8,6 @@ export HOROVOD_ROCM_PATH=/opt/rocm
 export HOROVOD_RCCL_HOME=/opt/rocm/rccl
 export RCCL_INCLUDE_DIRS=/opt/rocm/rccl/include
 export HOROVOD_RCCL_LIB=/opt/rocm/rccl/lib
-export HOROVOD_MPICXX_SHOW="CC --cray-print-opts=all"
 export HCC_AMDGPU_TARGET=gfx90a
 
 pip install --no-cache-dir --force-reinstall horovod==0.26.1
